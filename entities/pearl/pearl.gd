4bed4644adff8a1e5b97d5a8d846a2c7131b5fe3
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-05-08 22:30:11 -0400

    Added Subtle Bobbing Animation

--- entities/pearl/pearl.gd
+++ entities/pearl/pearl.gd
@@ -14,6 +14,9 @@ enum PearlVariant {
 }
 
 # --- Variables --- #
+const BOB_RANGE := 1.0
+const BOB_TIME := 2.0
+
 ## The variant of pearl this instance is. This determines the sprite and sell price
 @export var variant := PearlVariant.WHITE:
 	set(_var):
@@ -21,9 +24,26 @@ enum PearlVariant {
 		
 		$'sprite'.frame = int(variant)
 
+## The original position this pearl is in. This is used for anchoring the bobbing animation
+var origin: Vector2
+
 # --- Functions --- #
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	
+	origin = position
+	do_bob()
+
+func do_bob() -> void:
+	var new_pos := origin + Vector2(randf_range(-1, 1), randf_range(-1, 1)) * BOB_RANGE
+	
+	var tween := create_tween()
+	
+	tween.tween_property(self, ^'position', new_pos, BOB_TIME)
+	
+	await tween.finished
+	
+	do_bob()
 
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group(&'player'):
