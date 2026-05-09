f24b3276423f5e2cfe0ad1115c54aa52047c844a
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-05-08 22:51:58 -0400

    Added Pearl Collection Animation

--- entities/pearl/pearl.gd
+++ entities/pearl/pearl.gd
@@ -14,22 +14,33 @@ enum PearlVariant {
 }
 
 # --- Variables --- #
+## How far the pearl can get away from its origin point
 const BOB_RANGE := 1.0
+## How long it takes for the pearl to get from 1 point to another
 const BOB_TIME := 2.0
+
+## Whether or not this pearl is currently collected
+var collected := false
+
+@export var sparkle_variants: Dictionary[PearlVariant, Texture2D] = {}
 
 ## The variant of pearl this instance is. This determines the sprite and sell price
 @export var variant := PearlVariant.WHITE:
 	set(_var):
 		variant = _var
 		
-		$'sprite'.frame = int(variant)
+		setup_variant()
 
 ## The original position this pearl is in. This is used for anchoring the bobbing animation
 var origin: Vector2
 
 # --- Functions --- #
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	
 	body_entered.connect(_on_body_entered)
+	setup_variant()
 	
 	origin = position
 	do_bob()
@@ -49,8 +60,27 @@ func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group(&'player'):
 		return
 	
+	# don't collect twice
+	if collected:
+		return
+	
 	# collect pearl
+	collected = true
 	CurrencyManager.collect_pearl(variant)
 	
 	# play collection animation
+	$'particles'.emitting = true
+	
+	var tween := create_tween().set_parallel()
+	
+	tween.tween_property(self, ^'scale', Vector2.ZERO, 0.15)
+	tween.tween_property(self, ^'global_position', Globals.player.global_position, 0.15)
+	
+	await get_tree().create_timer(1.0).timeout
+	
+	# free pearl
 	queue_free()
+
+func setup_variant() -> void:
+	$'sprite'.frame = int(variant)
+	$'particles'.texture = sparkle_variants[variant]
