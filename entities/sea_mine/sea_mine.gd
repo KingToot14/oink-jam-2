7261d8eb690f459f2a30bd22e452cee84ba0268a
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2026-05-11 12:55:02 -0400

    Added Sea Mine Explosion
    Sea mines now explode when the player gets close

--- entities/sea_mine/sea_mine.gd
+++ entities/sea_mine/sea_mine.gd
@@ -13,6 +13,22 @@ var origin: Vector2
 # --- Functions --- #
 func _ready() -> void:
 	origin = position
+	
+	# reset damage radius
+	$'damage_source/shape'.shape.radius = 1.0
+	
+	# connect signals
+	$'explosion_range'.area_entered.connect(_on_area_entered)
 
 func _process(_delta: float) -> void:
 	global_position.x = origin.x + sin(((Time.get_ticks_msec() / 1000.0) + origin.x + origin.y) * BOB_TIME)
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is not Hitbox:
+		return
+	
+	explode()
+
+## Handles the animation and logic for the sea mine exploding
+func explode() -> void:
+	$'animator'.play(&'explode')
